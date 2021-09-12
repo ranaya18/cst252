6275d13e134a71e5124bc06dfd6055445393c5a9
ranaya18 <70286070+ranaya18@users.noreply.github.com>
2021-09-12 13:12:10 -0700

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+//Make the board before every game
+  //Loop through every letter in the name
+    // Write correct letter on space
+    // Draw each body part for wrong letter
+  //Make new line spaces for new name
+  //Make arch for the body
