a3f0143792dddd802ae41f6338c7e5893ffe5ed5
ranaya18 <70286070+ranaya18@users.noreply.github.com>
2021-09-14 20:22:48 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,4 +1,5 @@
 /*
+* lab5.js - Data Types and Variables
 * Author: Raeann Anaya raanaya@csumb.edu>
 * Created: 12 September
 * License: Public Domain
@@ -11,11 +12,15 @@ color = "Baby Blue";
 year = 2013;
 
 // Calculate
-age = 8;
+age = 2021 - 2013;
+
+// Declare Boolean Variable
+var ownIt = true;
 
 // Output
 document.writeln("Make: " + make + "<br>");
 document.writeln("Model: " + model + "<br>");
 document.writeln("Color: " + color + "<br>");
 document.writeln("Year: " + year + "<br>");
-document.writeln("Age: " + age + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("Own It: " + ownIt + "<br>");
