edef201b049df0147eeeebe515b972705fa1391e
ranaya18 <70286070+ranaya18@users.noreply.github.com>
2021-10-03 20:02:46 -0700

    lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* lab9.js - Anon Functions and Callbacks
+* Author: Raeann Anaya raanaya@csumb.edu>
+* Created: 2 October
+* License: Public Domain
+*/
+
+var oneEl = document.createElementById("outputEl");
+
+var twoEl = document.createElement("div");
+twoEl.style.color = "red";
+
+onnEl.appendChild(twoEl);
+
+var buttonElement = document.createElement("div")
+
+buttonElement.id = "button"
+
+buttonElement.innerHTML = "Press Me!";
+
+document.body.appendChild(buttonElement);
