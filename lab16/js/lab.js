b18fdfe87a25d06ace40b34df764e6bd4e8f395d
ranaya18 <70286070+ranaya18@users.noreply.github.com>
2021-11-01 01:22:41 -0700

    lab16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -5,6 +5,17 @@
 * License: Public Domain
 */
 
+function Vehicle(newmake, newmodel, newyear, newcolor){
+  this.make = newmake;
+  this.model = newmodel;
+  this.year = newyear;
+  this.color = newcolor;
+  this.info = function() {
+    return("I have a " + this.make + " " + this.model + " in the color " + this.color + " that was made in " + this.year);
+  }
+}
+
 var car = new Vehicle("Mini", "Cooper", 2013, "baby blue");
 
-document.getElementById("output").innerHTML += "<p>" + car.info() + "</p>";
+
+$("#output").append("<p>" + car.info() + "</p>");
