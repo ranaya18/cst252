eb6f6af5ece275253475819e4207bcb8461235c9
ranaya18 <70286070+ranaya18@users.noreply.github.com>
2021-11-15 00:56:31 -0800

    lab19

--- lab19/js/lab.js
+++ lab19/js/lab.js
@@ -4,3 +4,21 @@
 * Created: 15 November
 * License: Public Domain
 */
+
+$("button").click(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "/pages/test/",
+        data: {
+            id: $(this).val(), // < note use of 'this' here
+            access_token: $("#access_token").val()
+        },
+        success: function(result) {
+            alert('ok');
+        },
+        error: function(result) {
+            alert('Heyo!');
+        }
+    });
+});
